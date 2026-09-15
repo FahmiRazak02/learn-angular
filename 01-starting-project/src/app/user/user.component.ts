31a4f7d1b2efff9ade8cb66fe28507e84a2e1eab
Aiman Razak <aiman_razak@epam.com>
2026-09-15 12:30:10 +0800

    @Input to angular input signal approach

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, input, Input, signal } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,12 +7,19 @@ import { Component, computed, Input, signal } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath() {
-    return "assets/users/" + this.avatar;
-  }
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return "assets/users/" + this.avatar();
+  })
+
+  // get imagePath() {
+  //   return "assets/users/" + this.avatar;
+  // }
 
   onSelectedUser() {
 
