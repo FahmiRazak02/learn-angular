a8b9d7c4b5a2659405fe2ca6cd453a81b9dc5754
Aiman Razak <aiman_razak@epam.com>
2026-09-16 13:32:22 +0800

    revert

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, EventEmitter, input, Input, output, Output, signal } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, Output, signal } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,7 +10,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  select = output<string>();
+  @Output() select = new EventEmitter();
 
   get imagePath() {
     return "assets/users/" + this.avatar;
