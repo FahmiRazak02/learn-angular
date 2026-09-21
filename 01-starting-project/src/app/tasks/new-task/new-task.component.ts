e9502c5135c9c7755f1cd4e165a3b35380ec353c
Malvada <malvadatech@gmail.com>
2026-09-22 07:45:44 +0800

    complete: 02-55

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTask } from './new-task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -9,12 +11,27 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
   @Output() close = new EventEmitter<void>();
+  private tasksService = inject(TasksService);
+
   title = '';
   summary = '';
-  date = ''
+  date = '';
 
   onCancel = () => {
     this.close.emit();
-  }
+  };
+
+  onSubmit = () => {
+    this.tasksService.addTask(
+      {
+        title: this.title,
+        summary: this.summary,
+        date: this.date,
+      },
+      this.userId,
+    );
+    this.close.emit();
+  };
 }
