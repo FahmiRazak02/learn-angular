a974dc50d79bdd4a60b204d70a4387877da62028
Malvada <malvadatech@gmail.com>
2026-09-21 09:55:48 +0800

    feat: 2 way binding with ngModel

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
   @Output() close = new EventEmitter<void>();
+  title = '';
+  summary = '';
+  date = ''
+
+  onCancel = () => {
+    this.close.emit();
+  }
 }
